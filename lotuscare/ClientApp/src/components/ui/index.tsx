b2cad02832f2607e52876a0f217db34712e95fc4
nhudpq <141238502+nhudpq@users.noreply.github.com>
2026-03-27 16:40:22 +0700

    fix: update ui

---
+++ lotuscare/ClientApp/src/components/ui/index.tsx
@@ -0,0 +1,6 @@
+export { Input } from "./input";
+export { Separator } from "./separator";
+export { Skeleton } from "./skeleton";
+export { Sheet, SheetContent, SheetTrigger } from "./sheet";
+
+export { Button } from "./button";
--- lotuscare/ClientApp/vite.config.ts
+++ lotuscare/ClientApp/vite.config.ts
@@ -1,3 +1,4 @@
+import path from "path"
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
@@ -11,5 +12,10 @@ export default defineConfig({
   server: {
     port: 5173,
     host: 'localhost'
-  }
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 })
